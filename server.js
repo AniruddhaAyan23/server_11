23bdab73e5aaed17c0b0aea7e9e20fa0aa62280a
AniruddhaAyan23 <7.aniruddha@gmail.com>
2025-12-22 09:53:11 +0600

    imported routes

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import authRoutes from './routes/auth.js';
+import assetRoutes from './routes/assets.js';
+import requestRoutes from './routes/requests.js';
+import employeeRoutes from './routes/employees.js';
+import packageRoutes from './routes/packages.js';
+import paymentRoutes from './routes/payments.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
